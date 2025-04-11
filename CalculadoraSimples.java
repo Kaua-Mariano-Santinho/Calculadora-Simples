305af9c12926f035861ba4345c93889e14f93b1f
Jao <dasilvajao639@gmail.com>
2025-04-11 16:20:40 -0300

    Commit do tio. - Laço de repetição e switch ... case

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -8,52 +8,65 @@ public class CalculadoraSimples {
         // Criação e Instância do Obj. de Entrada
         Scanner entrada = new Scanner(System.in);
 
-        // Apresentação
-        System.out.println("\n\t\t\t -- Calculadora Simples --\n");
+        do{
+            // Apresentação
+            System.out.println("\n\t\t\t -- Calculadora Simples --\n");
 
-        // Menu
-        System.out.println("1. Soma");
-        System.out.println("2. Subtração");
-        System.out.println("3. Sair");
+            // Menu
+            System.out.println("1. Soma");
+            System.out.println("2. Subtração");
+            System.out.println("3. Sair");
 
-        System.out.print("Opção: ");
-        op = entrada.nextInt();
+            System.out.print("Opção: ");
+            op = entrada.nextInt();
 
-        if (op == 1) {
-            // Entrada
-            System.out.println("Soma Simples: ");
-            System.out.println("Informe um número:");
-            n1 = entrada.nextInt();
-            System.out.println("Informe outro número:");
-            n2 = entrada.nextInt();
+            switch(op){
+            //if (op == 1) { obsoleto
+                case 1:
+                    // Entrada
+                    System.out.println("Soma Simples: ");
+                    System.out.println("Informe um número:");
+                    n1 = entrada.nextInt();
+                    System.out.println("Informe outro número:");
+                    n2 = entrada.nextInt();
 
-            // Processamento
-            total = n1 + n2;
+                    // Processamento
+                    total = n1 + n2;
 
-            // Saída
-            System.out.println("O Calculo é: " + n1 + " + " + n2 + " = " + total);
-        }
+                    // Saída
+                    System.out.println("O Calculo é: " + n1 + " + " + n2 + " = " + total);
+                    break;
+            //}
 
-        if (op == 2) {
-            // Entrada
-            System.out.println("Subtração Simples: ");
-            System.out.println("Informe um número:");
-            n1 = entrada.nextInt();
-            System.out.println("Informe outro número:");
-            n2 = entrada.nextInt();
+            //if (op == 2) {
+                case 2:
+                    // Entrada
+                    System.out.println("Subtração Simples: ");
+                    System.out.println("Informe um número:");
+                    n1 = entrada.nextInt();
+                    System.out.println("Informe outro número:");
+                    n2 = entrada.nextInt();
 
-            // Processamento
-            total = n1 - n2;
+                    // Processamento
+                    total = n1 - n2;
 
-            // Saída
-            System.out.println("O Calculo é: " + n1 + " - " + n2 + " = " + total);
-        }
-        if (op == 3) {
-            System.out.println("\n Forte Abraço");
-        }
-        else if(op >= 3) {
-            System.out.println("Escolha uma opção correta..."); 
-        }
+                    // Saída
+                    System.out.println("O Calculo é: " + n1 + " - " + n2 + " = " + total);
+                    break;
+            //}
+            //if (op == 3) {
+                case 3:
+                    System.out.println("\n Forte Abraço");
+                    break;
+            //}
+            //else if(op >= 3) {
+                default:
+                    System.out.println("Escolha uma opção correta...");
+
+            //}
+            }
+
+        }while(op != 3);
 
     }    
 }
